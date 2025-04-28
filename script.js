d81ed9d1541dc1b11a18294f5356708957571331
Jure Sulič <j.sulic@gmail.com>
2025-04-28 20:10:04 +0300

    Added object

--- script.js
+++ script.js
@@ -122,6 +122,7 @@ function init() {
   // Load 3D model (GLTF)
   loadModel('winnowing_basket_sim.gltf', "Winnowing_Basket_LP", new THREE.Vector3(0, 0, 0));
   loadModel('ox_thing.gltf', "Ox_Thing", new THREE.Vector3(3, 0, 0));
+  loadModel('brana.gltf', "Brana", new THREE.Vector3(0, 0, 3));
   modelGridArray('walls.gltf', new THREE.Vector3(0, 0, 0), new THREE.Vector3(3, 0, 3), new THREE.Vector3(27, 1, 27), new THREE.Vector3(0.2, 0.2, 0.2));
   modelGridArray('candle.gltf', new THREE.Vector3(0, 0, 0), new THREE.Vector3(3, 0, 3), new THREE.Vector3(27, 1, 27), new THREE.Vector3(0.02, 0.2, 0.02 ));
   modelGridArray('ground.gltf', new THREE.Vector3(0, 0, 0), new THREE.Vector3(3, 0, 3), new THREE.Vector3(27, 1, 27));
